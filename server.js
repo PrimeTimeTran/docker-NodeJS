63c54c7f36e3dfe8c9e15eb472204f231cd0e8c9
Loi Van Tran <datloiboi@gmail.com>
2021-09-04 06:55:59 +0700

    Update app

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/foo", (req, res) => {
+  res.send("Hello World " + `http://${HOST}:${PORT}`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
